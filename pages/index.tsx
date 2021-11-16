aaf966380c17a4bfb036150879c6662e4b40b52a
yeabu369 <yeabsdebe@gmail.com>
2021-11-16 13:30:39 +0300

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>DAEX</title>
+        <meta name="description" content="Digital Asset Ethiopia Exchange" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+    </>
+  );
+};
+
+export default Home;
