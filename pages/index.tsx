d6fe9152e9b7d1bb8405726a000a87670f1fb485
yeabu369 <yeabsdebe@gmail.com>
2021-11-18 09:23:58 +0300

    Added USDT Logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   const [user, setUser] = useState(null);
@@ -12,8 +13,8 @@ const Home: NextPage = () => {
         <meta name="description" content="Digital Asset Ethiopia Exchange" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex flex-col border sm:px-24 px-12">
-        <div className="border w-full mt-20">
+      <main className="flex flex-col sm:px-24 px-12">
+        <div className=" w-full mt-20">
           <div>
             <h1 className="text-5xl font-medium">
               Buy {"&"} Sell Crypto with ETB
@@ -28,26 +29,34 @@ const Home: NextPage = () => {
           </div>
         </div>
 
-        <section className="border mt-20">
+        <section className=" mt-20">
           <div className="mb-8">
             <h2 className="text-3xl">Markets</h2>
           </div>
-          <div className="flex border justify-between p-4">
-            <div className="border flex flex-grow">
+          <div className="flex justify-between p-4">
+            <div className=" flex flex-grow">
               <div className="text-sm font-medium">Name</div>
             </div>
-            <div className="border flex flex-grow justify-end">
+            <div className=" flex flex-grow sm:justify-center justify-end">
               <div className="text-sm font-medium">Last Price</div>
             </div>
-            <div className="border flex flex-grow justify-end">
+            <div className=" flex flex-grow justify-end">
               <div className="text-sm font-medium">24h Change</div>
             </div>
           </div>
           <Link href="/trade/USDT_cETB">
-            <a className="flex justify-between p-4 hover:bg-gray-200 rounded-sm">
-              <div className="flex flex-grow">Tether USD</div>
-              <div className="flex flex-grow justify-end">ETB 60.54</div>
-              <div className="flex flex-grow justify-end">-1.25%</div>
+            <a className="flex justify-between p-4 hover:bg-gray-200 rounded-md">
+              <div className="flex flex-1 items-center">
+                <div className="">
+                  <Image src="/USDT-Logo.svg" height={32} width={32}></Image>
+                </div>
+                <div className="ml-4 md:flex ">
+                  <div className="text-xl font-normal sm:mr-2">USDT</div>
+                  <div className="text-sm md:text-xl text-gray-600">Tether-USD</div>
+                </div>
+              </div>
+              <div className="flex justify-end sm:flex-grow-0 items-center ">ETB 60.54</div>
+              <div className="flex flex-1 justify-end items-center ">-1.25%</div>
             </a>
           </Link>
         </section>
