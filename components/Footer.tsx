5c464e59fc7b07ae46b466b70c1a02572721c84d
yeabu369 <yeabsdebe@gmail.com>
2021-11-18 09:33:14 +0300

    Fixing footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/Home.module.css";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
+    <footer className="flex flex-1 justify-center items-center py-8 mt-20 border-t">
         Made with ♥️ by Ethiopians!
     </footer>
   );
