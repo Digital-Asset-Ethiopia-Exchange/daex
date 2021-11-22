164496961d3f03a2eb778a3fea3f01232cc1e26b
yeabu369 <yeabsdebe@gmail.com>
2021-11-22 11:02:57 +0300

    Added limit order page

--- pages/trade/[id].tsx
+++ pages/trade/[id].tsx
@@ -15,20 +15,11 @@ const Trade = () => {
       { i: "trade-history", x: 2, y: 0, w: 2, h: 9 },
       { i: "order-history", x: 4, y: 0, w: 6, h: 9 },
     ],
-    md: [
-      { i: "limit-order", x: 0, y: 0, w: 10, h: 18, static: true },
-      { i: "order-book", x: 3, y: 0, w: 3, h: 15, static: true },
-      { i: "market-summary", x: 6, y: 0, w: 6, h: 2 },
-      { i: "chart", x: 6, y: 0, w: 6, h: 13 },
-      { i: "asset-summary", x: 0, y: 0, w: 3, h: 6 },
-      { i: "trade-history", x: 3, y: 0, w: 3, h: 9 },
-      { i: "order-history", x: 6, y: 0, w: 6, h: 9 },
-    ],
     sm: [
       { i: "trade-history", x: 0, y: 35, w: 2, h: 9, static: true },
       { i: "order-history", x: 0, y: 0, w: 6, h: 9, static: true },
       { i: "order-book", x: 0, y: 23, w: 2, h: 12, static: true },
-      { i: "asset-summary", x: 0, y: 0, w: 0, h: 0, static: true},
+      { i: "asset-summary", x: 0, y: 0, w: 0, h: 0, static: true },
       { i: "chart", x: 0, y: 14, w: 6, h: 9, static: true },
       { i: "limit-order", x: 0, y: 2, w: 2, h: 12, static: true },
       { i: "market-summary", x: 0, y: 0, w: 6, h: 2, static: true },
@@ -45,8 +36,45 @@ const Trade = () => {
       containerPadding={[0, 0]}
       margin={[0, 0]}
     >
-      <div key="limit-order" className="border-2">
-        Limit Order
+      <div key="limit-order" className="border-2 bg-gray-100">
+        <div className="pt-6 px-4 border h-full">
+          <div className="border-red-300 border">
+            <div className="h-10 border flex">
+              <button className="border rounded-md flex-auto bg-light-green text-white font-bold">
+                BUY
+              </button>
+              <button className="rounded-md flex-auto border-black border-2 font-bold">
+                SELL
+              </button>
+            </div>
+            <div className="border mt-5">
+              <div className="border font-bold mb-2">
+                <h1>Limit Order</h1>
+              </div>
+              <div className="border">
+                <h2 className="text-xs">
+                  Available <span className="font-bold">0.00000000 ETB</span>
+                </h2>
+              </div>
+            </div>
+            <div className="border mt-4">
+              <form className="border">
+                <div>
+                  <div className="group border rounded-md flex items-center justify-center h-12 bg-white focus:ring-2 focus:ring-blue-500 px-4">
+                    <div className="">
+                      <label htmlFor="FormRow-Buy-Price">Price</label>
+                    </div>
+                    <input type="number" name="price" id="FormRow-Buy-price" step="0.01" min="" className="text-right h-full focus:outline-none" />
+                    <div>
+                      <label htmlFor="FormRow-Buy-price">ETB</label>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="h-8 border"></div>
+          </div>
+        </div>
       </div>
       <div key="order-book" className="border-2 lg">
         order-book
