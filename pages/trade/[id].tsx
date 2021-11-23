17a61572e02c1638468cc1c352e0c51ac928587b
yeabu369 <yeabsdebe@gmail.com>
2021-11-23 09:41:11 +0300

    Cleaning up and added Sell Form

--- pages/trade/[id].tsx
+++ pages/trade/[id].tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Responsive, WidthProvider } from "react-grid-layout";
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
@@ -5,6 +6,8 @@ import "/node_modules/react-resizable/css/styles.css";
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const Trade = () => {
+  const [buyerActive, setBuyerActive] = useState<Boolean>(true);
+
   const layouts = {
     lg: [
       { i: "limit-order", x: 0, y: 0, w: 2, h: 18, static: true },
@@ -37,89 +40,207 @@ const Trade = () => {
       margin={[0, 0]}
     >
       <div key="limit-order" className="bg-gray-100">
-        <div className="pt-6 px-4 border h-full">
-          <div className="border-red-300 border">
-            <div className="h-10 border flex">
-              <button className="border rounded-md flex-auto bg-light-green text-white font-bold">
-                BUY
-              </button>
-              <button className="rounded-md flex-auto border-black border-2 font-bold">
-                SELL
-              </button>
-            </div>
-            <div className="border mt-5">
-              <div className="border font-bold mb-2">
-                <h1>Limit Order</h1>
+        {buyerActive ? (
+          <div className="pt-6 px-4 h-full">
+            <div>
+              <div className="h-10 flex">
+                <button
+                  onClick={() => setBuyerActive(!buyerActive)}
+                  className="rounded-md flex-auto bg-light-green text-white font-bold"
+                >
+                  BUY
+                </button>
+                <button
+                  onClick={() => setBuyerActive(!buyerActive)}
+                  className="rounded-md flex-auto bg-gray-200 border-gray-500 text-gray-400 font-bold"
+                >
+                  SELL
+                </button>
               </div>
-              <div className="border">
-                <h2 className="text-xs">
-                  Available <span className="font-bold">0.00000000 ETB</span>
-                </h2>
+              <div className="mt-5">
+                <div className="font-bold mb-2">
+                  <h1>Limit Order</h1>
+                </div>
+                <div>
+                  <h2 className="text-xs">
+                    Available <span className="font-bold">0.00000000 ETB</span>
+                  </h2>
+                </div>
               </div>
-            </div>
-            <div className="border mt-4">
-              <form>
-                <div className="rounded-md flex items-center justify-center h-12 bg-white px-2">
-                  <div className="flex items-center h-full text-sm font-bold text-gray-500">
-                    <label htmlFor="FormRow-Buy-Price">Price</label>
+              <div className="mt-4">
+                <form>
+                  <div className="rounded-md flex items-center justify-center h-12 bg-white px-2">
+                    <div className="flex items-center h-full text-sm font-bold text-gray-500">
+                      <label htmlFor="FormRow-Buy-Price">Price</label>
+                    </div>
+                    <input
+                      type="number"
+                      name="price"
+                      id="FormRow-Buy-price"
+                      step="0.01"
+                      min="0"
+                      className="text-right outline-none h-full w-full"
+                    />
+                    <div className="px-2 text-sm font-bold">
+                      <label htmlFor="FormRow-Buy-price">ETB</label>
+                    </div>
                   </div>
-                  <input
-                    type="number"
-                    name="price"
-                    id="FormRow-Buy-price"
-                    step="0.01"
-                    min="0"
-                    className="text-right outline-none h-full w-full"
-                  />
-                  <div className="px-2 text-sm font-bold">
-                    <label htmlFor="FormRow-Buy-price">ETB</label>
+                  <div className="rounded-md flex items-center justify-center h-12 bg-white px-2 mt-4">
+                    <div className="flex items-center h-full text-sm font-bold text-gray-500">
+                      <label htmlFor="FormRow-Buy-Amount">Amount</label>
+                    </div>
+                    <input
+                      type="number"
+                      name="amount"
+                      id="FormRow-Buy-Amount"
+                      step="0.01"
+                      min="0"
+                      className="text-right outline-none h-full w-full"
+                    />
+                    <div className="px-2 text-sm font-bold">
+                      <label htmlFor="FormRow-Buy-Amount">USDT</label>
+                    </div>
                   </div>
-                </div>
-                <div className="rounded-md flex items-center justify-center h-12 bg-white px-2 mt-4">
-                  <div className="flex items-center h-full text-sm font-bold text-gray-500">
-                    <label htmlFor="FormRow-Buy-Amount">Amount</label>
+                  <div className="border mt-5 text-gray-400 flex text-center space-x-3 text-xs">
+                    <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">
+                      25%
+                    </a>
+                    <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">
+                      50%
+                    </a>
+                    <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">
+                      75%
+                    </a>
+                    <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">
+                      100%
+                    </a>
                   </div>
-                  <input
-                    type="number"
-                    name="amount"
-                    id="FormRow-Buy-Amount"
-                    step="0.01"
-                    min="0"
-                    className="text-right outline-none h-full w-full"
-                  />
-                  <div className="px-2 text-sm font-bold">
-                    <label htmlFor="FormRow-Buy-Amount">USDT</label>
+                  <div className="rounded-md flex items-center justify-center h-12 bg-white px-2 mt-4">
+                    <div className="flex items-center h-full text-sm font-bold text-gray-500">
+                      <label htmlFor="FormRow-Buy-Total">Total</label>
+                    </div>
+                    <input
+                      type="number"
+                      name="total"
+                      id="FormRow-Buy-Total"
+                      step="0.01"
+                      min="0"
+                      className="text-right outline-none h-full w-full"
+                    />
+                    <div className="px-2 text-sm font-bold">
+                      <label htmlFor="FormRow-Buy-Total">ETB</label>
+                    </div>
                   </div>
-                </div>
-                <div className="border mt-5 text-gray-400 flex text-center space-x-3 text-xs">
-                  <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">25%</a>
-                  <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">50%</a>
-                  <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">75%</a>
-                  <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">100%</a>
-                </div>
-                <div className="rounded-md flex items-center justify-center h-12 bg-white px-2 mt-4">
-                  <div className="flex items-center h-full text-sm font-bold text-gray-500">
-                    <label htmlFor="FormRow-Buy-Total">Total</label>
-                  </div>
-                  <input
-                    type="number"
-                    name="total"
-                    id="FormRow-Buy-Total"
-                    step="0.01"
-                    min="0"
-                    className="text-right outline-none h-full w-full"
-                  />
-                  <div className="px-2 text-sm font-bold">
-                    <label htmlFor="FormRow-Buy-Total">ETB</label>
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div className="border mt-4 text-white">
-              <button className="w-full h-8 bg-light-green font-semibold rounded-sm">BUY USDT</button>
+                </form>
+              </div>
+              <div className="border mt-4 text-white">
+                <button className="w-full h-8 bg-light-green font-semibold rounded-sm">
+                  BUY USDT
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="pt-6 px-4 h-full">
+            <div>
+              <div className="h-10 flex">
+                <button
+                  onClick={() => setBuyerActive(!buyerActive)}
+                  className="rounded-md flex-auto bg-gray-200 border-gray-500 text-gray-400 font-bold"
+                >
+                  BUY
+                </button>
+                <button
+                  onClick={() => setBuyerActive(!buyerActive)}
+                  className="rounded-md flex-auto bg-light-red font-bold text-white"
+                >
+                  SELL
+                </button>
+              </div>
+              <div className="mt-5">
+                <div className="font-bold mb-2">
+                  <h1>Limit Order</h1>
+                </div>
+                <div>
+                  <h2 className="text-xs">
+                    Available <span className="font-bold">0.00000000 USDT</span>
+                  </h2>
+                </div>
+              </div>
+              <div className="mt-4">
+                <form>
+                  <div className="rounded-md flex items-center justify-center h-12 bg-white px-2">
+                    <div className="flex items-center h-full text-sm font-bold text-gray-500">
+                      <label htmlFor="FormRow-Sell-Price">Price</label>
+                    </div>
+                    <input
+                      type="number"
+                      name="price"
+                      id="FormRow-Sell-price"
+                      step="0.01"
+                      min="0"
+                      className="text-right outline-none h-full w-full"
+                    />
+                    <div className="px-2 text-sm font-bold">
+                      <label htmlFor="FormRow-Sell-price">ETB</label>
+                    </div>
+                  </div>
+                  <div className="rounded-md flex items-center justify-center h-12 bg-white px-2 mt-4">
+                    <div className="flex items-center h-full text-sm font-bold text-gray-500">
+                      <label htmlFor="FormRow-Sell-Amount">Amount</label>
+                    </div>
+                    <input
+                      type="number"
+                      name="amount"
+                      id="FormRow-Sell-Amount"
+                      step="0.01"
+                      min="0"
+                      className="text-right outline-none h-full w-full"
+                    />
+                    <div className="px-2 text-sm font-bold">
+                      <label htmlFor="FormRow-Sell-Amount">USDT</label>
+                    </div>
+                  </div>
+                  <div className="border mt-5 text-gray-400 flex text-center space-x-3 text-xs">
+                    <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">
+                      25%
+                    </a>
+                    <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">
+                      50%
+                    </a>
+                    <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">
+                      75%
+                    </a>
+                    <a className=" border-t-8 pt-1 border-gray-400 font-bold w-full hover:border-light-green hover:text-light-green cursor-pointer">
+                      100%
+                    </a>
+                  </div>
+                  <div className="rounded-md flex items-center justify-center h-12 bg-white px-2 mt-4">
+                    <div className="flex items-center h-full text-sm font-bold text-gray-500">
+                      <label htmlFor="FormRow-Sell-Total">Total</label>
+                    </div>
+                    <input
+                      type="number"
+                      name="total"
+                      id="FormRow-Sell-Total"
+                      step="0.01"
+                      min="0"
+                      className="text-right outline-none h-full w-full"
+                    />
+                    <div className="px-2 text-sm font-bold">
+                      <label htmlFor="FormRow-Sell-Total">ETB</label>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div className="border mt-4 text-white">
+                <button className="w-full h-8 bg-light-red font-semibold rounded-sm">
+                  SELL USDT
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
       <div key="order-book" className="border-2 lg">
         order-book
